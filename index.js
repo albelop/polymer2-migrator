153e3fb24096919bd119575c0074fd08aa0c62e3
Alberto <alberto.lopezbarranco@gmail.com>
2017-09-21 17:17:44 +0200

    Improved FP

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const argv = require('yargs').argv;
 const parse5 = require('parse5');
 const fs = require('fs');
+const lodash = require('lodash/fp');
 
 /* Function definitions */
 
@@ -12,8 +13,13 @@ const removeRootCSS = (style) => style.replace(/:root/g, ':host > *');
 const fixCssDefaultVal = (style) => style.replace(/var\((.*), *(--.*)\)/g, 'var($1, var($2))');
 const fixCssApply = (style) => style.replace(/@apply\((.*?)\)/g, '@apply $1');
 
+const fixCss = (styleNode) => {
+	styleNode.data = fixCssApply(fixCssDefaultVal(removeRootCSS(styleNode.data)));
+	return styleNode;
+}
+
 const callback = (elem) => {
-  let newElement = Object.assign({},elem);
+	let newElement = lodash.cloneDeep(elem);
 	switch (elem.name) {
 		case 'dom-module':
 			newElement.attribs = setDomModuleId(elem.attribs);
@@ -23,15 +29,14 @@ const callback = (elem) => {
 			newElement.attribs = setSlot(elem.attribs);
 			break;
 		case 'style':
-			newElement.children.forEach((styleNode) => {
-				styleNode.data = fixCssApply(fixCssDefaultVal(removeRootCSS(styleNode.data)))
-			});
+			newElement.children = elem.children.map(fixCss);
+			break;
 	}
-  return newElement;
+	return newElement;
 };
 
 const setSlot = (attrs) => {
-	let newAttrs = Object.assign({},attrs);
+	let newAttrs = lodash.cloneDeep(attrs);
 	if ('select' in attrs) {
 		newAttrs.name = parseSelector(attrs.select);
 		delete(newAttrs.select);
@@ -41,7 +46,7 @@ const setSlot = (attrs) => {
 };
 
 const setDomModuleId = (attrs) => {
-	let newAttrs = Object.assign({},attrs);
+	let newAttrs = lodash.cloneDeep(attrs);
 	if ('is' in attrs || 'name' in attrs) {
 		newAttrs.id = attrs.is || attrs.name;
 		delete(newAttrs.is);
@@ -51,10 +56,10 @@ const setDomModuleId = (attrs) => {
 };
 
 const traverseItem = (node) => {
-  let newNode = Object.assign({},callback(node));
-	newNode.children = !!newNode.children?newNode.children.map(traverseItem):null;
+	let newNode = Object.assign({}, callback(node));
+	newNode.children = !!newNode.children ? newNode.children.map(traverseItem) : null;
 
-  return newNode
+	return newNode
 };
 
 /* App */
@@ -68,7 +73,8 @@ const src = fs.readFile(inputFile, 'utf8', function(err, html) {
 		const tree = parse5.parseFragment(html, {
 			treeAdapter: parse5.treeAdapters.htmlparser2
 		});
-		let modifiedTree = traverseItem(tree);
+
+		const modifiedTree = traverseItem(tree);
 		const serializer = new parse5.SerializerStream(modifiedTree, {
 			treeAdapter: parse5.treeAdapters.htmlparser2
 		});
