93673788d436d95cd47b4ababfc65e515dd03639
Alberto <alberto.lopezbarranco@gmail.com>
2017-09-21 09:33:31 +0200

    Refactored recursive function to make it pure.

--- index.js
+++ index.js
@@ -2,69 +2,77 @@ const argv = require('yargs').argv;
 const parse5 = require('parse5');
 const fs = require('fs');
 
+/* Function definitions */
+
 const inputFile = argv.file;
-
-const isType = (...types)=>(e)=>types.includes(e.type);
-
+const isType = (...types) => (e) => types.includes(e.type);
 const parseSelector = (selector) => selector.match(/([\w-]+)/g).join('-');
 
-const removeRootCSS = (style) => style.replace(/:root/g,':host > *');
-const fixCssDefaultVal = (style) => style.replace(/var\((.*), *(--.*)\)/g,'var($1, var($2))');
-const fixCssApply = (style) => style.replace(/@apply\((.*?)\)/g,'@apply $1');
+const removeRootCSS = (style) => style.replace(/:root/g, ':host > *');
+const fixCssDefaultVal = (style) => style.replace(/var\((.*), *(--.*)\)/g, 'var($1, var($2))');
+const fixCssApply = (style) => style.replace(/@apply\((.*?)\)/g, '@apply $1');
 
-const callback = (e)=> {
-    switch (e.name) {
-        case 'dom-module':
-            e.attribs = setDomModuleId(e.attribs);
-            break;
-        case 'content':
-            e.name = 'slot';
-            e.attribs = setSlot(e.attribs);
-            break;
-        case 'style':
-            e.children.forEach((styleNode)=>{styleNode.data=fixCssApply(fixCssDefaultVal(removeRootCSS(styleNode.data)))});
-    }
+const callback = (elem) => {
+  let newElement = Object.assign({},elem);
+	switch (elem.name) {
+		case 'dom-module':
+			newElement.attribs = setDomModuleId(elem.attribs);
+			break;
+		case 'content':
+			newElement.name = 'slot';
+			newElement.attribs = setSlot(elem.attribs);
+			break;
+		case 'style':
+			newElement.children.forEach((styleNode) => {
+				styleNode.data = fixCssApply(fixCssDefaultVal(removeRootCSS(styleNode.data)))
+			});
+	}
+  return newElement;
 };
 
 const setSlot = (attrs) => {
-    let newAttrs = Object.assign(attrs);
-    if ('select' in attrs) {
-        newAttrs.name = parseSelector(attrs.select);
-        delete (newAttrs.select);
-    }
-    return newAttrs;
+	let newAttrs = Object.assign({},attrs);
+	if ('select' in attrs) {
+		newAttrs.name = parseSelector(attrs.select);
+		delete(newAttrs.select);
+	}
+	return newAttrs;
 
 };
 
-const setDomModuleId = (attrs)=> {
-    let newAttrs = Object.assign(attrs);
-    if ('is' in attrs || 'name' in attrs) {
-        newAttrs.id = attrs.is || attrs.name;
-        delete (newAttrs.is);
-        delete (newAttrs.name);
-    }
-    return newAttrs;
+const setDomModuleId = (attrs) => {
+	let newAttrs = Object.assign({},attrs);
+	if ('is' in attrs || 'name' in attrs) {
+		newAttrs.id = attrs.is || attrs.name;
+		delete(newAttrs.is);
+		delete(newAttrs.name);
+	}
+	return newAttrs;
 };
 
-const traverseItem = (node)=> {
-    callback(node);
+const traverseItem = (node) => {
+  let newNode = Object.assign({},callback(node));
+	newNode.children = !!newNode.children?newNode.children.map(traverseItem):null;
 
-    if (!!node.children) {
-        node.children
-            .forEach(traverseItem);
-    }
+  return newNode
 };
+
+/* App */
 
 const file = fs.createWriteStream('./output/' + inputFile);
 
-const src = fs.readFile(inputFile, 'utf8', function (err, html) {
-    if (err) {
-        // handle error
-    } else {
-        const tree = parse5.parseFragment(html, {treeAdapter: parse5.treeAdapters.htmlparser2});
-        traverseItem(tree);
-        const serializer = new parse5.SerializerStream(tree, {treeAdapter: parse5.treeAdapters.htmlparser2});
+const src = fs.readFile(inputFile, 'utf8', function(err, html) {
+	if (err) {
+		// handle error
+	} else {
+		const tree = parse5.parseFragment(html, {
+			treeAdapter: parse5.treeAdapters.htmlparser2
+		});
+		let modifiedTree = traverseItem(tree);
+		const serializer = new parse5.SerializerStream(modifiedTree, {
+			treeAdapter: parse5.treeAdapters.htmlparser2
+		});
 
-        serializer.pipe(file);
-    }
+		serializer.pipe(file);
+	}
 });
