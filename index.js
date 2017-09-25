54b33d7a273983f6d7f9233ec5f7e537b1146902
albelop <alberto.lopezbarranco@gmail.com>
2017-09-25 23:57:10 +0200

    Remove ::shadow and /deep/ styles

--- index.js
+++ index.js
@@ -10,87 +10,85 @@ const parseSelector = selector => selector.match(/([\w-]+)/g).join('-');
 const fixCssRoot = style => style.replace(/:root/g, ':host > *');
 const fixCssDefaultVal = style => style.replace(/var\((.*), *(--.*)\)/g, 'var($1, var($2))');
 const fixCssApply = style => style.replace(/@apply\((.*?)\)/g, '@apply $1');
+const isOldShadowStyle = styleNode => (styleNode.type === "PseudoElementSelector" && styleNode.name === "shadow") || (styleNode.type === "Combinator" && styleNode.name === "/deep/");
+
+
 const fixCssShadow = style => {
-	let tree = csstree.parse(style)
-	let removableRules = [];
-	csstree.walk(tree, function(node, item, list) {
-		if ((node.type == "PseudoElementSelector" && node.name == "shadow") | (node.type == "Combinator" && node.name == "/deep/")) {
-			removableRules.push(this.rule);
-		}
-	});
-	let tree2 = csstree.toPlainObject(csstree.clone(tree));
+    let tree = csstree.parse(style);
 
-	// console.log(removableRules)
-	tree2.children = tree2.children.map((e)=>e==removableRules[0])
-	console.log(tree2.children)
-	// let parsed = csstree.parse(style);
-// console.log(JSON.stringify(tree,null,2));
-	// console.log(csstree.translate(tree));
-	// parsed.cssRules = parsed.cssRules.filter((e)=>!(e.selectorText.includes('/deep/')|e.selectorText.includes('::shadow')));
-	return csstree.translate(tree);
+    let children= tree.children.toArray().filter(
+        rule => !rule.prelude.children.some(
+            ruleSelector => (
+                ruleSelector.children.some(
+                    isOldShadowStyle
+                )
+            )
+        )
+    );
+    tree.children =  tree.children.fromArray(children);
 
+    return csstree.translate(tree);
 
 };
 
 const fixCss = (styleNode) => {
-	let newStyleNode = lodash.cloneDeep(styleNode);
-	newStyleNode.data = lodash.compose(fixCssRoot, fixCssDefaultVal, fixCssApply, fixCssShadow)(newStyleNode.data);
-	return newStyleNode;
-}
+    let newStyleNode = lodash.cloneDeep(styleNode);
+    newStyleNode.data = lodash.compose(fixCssRoot, fixCssShadow, fixCssDefaultVal, fixCssApply)(newStyleNode.data);
+    return newStyleNode;
+};
 
 const upgradeNode = (elem) => {
-	let newElement = lodash.cloneDeep(elem);
-	switch (elem.name) {
-		case 'dom-module':
-			newElement.attribs = setDomModuleId(elem.attribs);
-			break;
-		case 'content':
-			newElement.name = 'slot';
-			newElement.attribs = setSlot(elem.attribs);
-			break;
-		case 'style':
-			newElement.children = elem.children.map(fixCss);
-			break;
-	}
-	return newElement;
+    let newElement = lodash.cloneDeep(elem);
+    switch (elem.name) {
+        case 'dom-module':
+            newElement.attribs = setDomModuleId(elem.attribs);
+            break;
+        case 'content':
+            newElement.name = 'slot';
+            newElement.attribs = setSlot(elem.attribs);
+            break;
+        case 'style':
+            newElement.children = elem.children.map(fixCss);
+            break;
+    }
+    return newElement;
 };
 
 const setSlot = (attrs) => {
-	let newAttrs = lodash.cloneDeep(attrs);
-	if ('select' in attrs) {
-		newAttrs.name = parseSelector(newAttrs.select);
-		delete(newAttrs.select);
-	}
-	return newAttrs;
+    let newAttrs = lodash.cloneDeep(attrs);
+    if ('select' in attrs) {
+        newAttrs.name = parseSelector(newAttrs.select);
+        delete(newAttrs.select);
+    }
+    return newAttrs;
 
 };
 
 const setDomModuleId = (attrs) => {
-	let newAttrs = lodash.cloneDeep(attrs);
-	if ('is' in newAttrs || 'name' in newAttrs) {
-		newAttrs.id = attrs.is || newAttrs.name;
-		delete(newAttrs.is);
-		delete(newAttrs.name);
-	}
-	return newAttrs;
+    let newAttrs = lodash.cloneDeep(attrs);
+    if ('is' in newAttrs || 'name' in newAttrs) {
+        newAttrs.id = attrs.is || newAttrs.name;
+        delete(newAttrs.is);
+        delete(newAttrs.name);
+    }
+    return newAttrs;
 };
 
 const traverseItem = (node) => {
-	let newNode = lodash.cloneDeep(node);
-	newNode = upgradeNode(newNode);
-	newNode.children = !!newNode.children ? newNode.children.map(traverseItem) : null;
-	return newNode
+    let newNode = lodash.cloneDeep(node);
+    newNode = upgradeNode(newNode);
+    newNode.children = !!newNode.children ? newNode.children.map(traverseItem) : null;
+    return newNode
 };
 
-
 module.exports = {
-	migrate: html => {
-		const tree = parse5.parseFragment(html, {
-			treeAdapter: parse5.treeAdapters.htmlparser2
-		});
-		const modifiedTree = traverseItem(tree);
-		return parse5.serialize(modifiedTree, {
-			treeAdapter: parse5.treeAdapters.htmlparser2
-		});
-	}
+    migrate: html => {
+        const tree = parse5.parseFragment(html, {
+            treeAdapter: parse5.treeAdapters.htmlparser2
+        });
+        const modifiedTree = traverseItem(tree);
+        return parse5.serialize(modifiedTree, {
+            treeAdapter: parse5.treeAdapters.htmlparser2
+        });
+    }
 }
