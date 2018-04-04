9e98fc74df36b0afd960fbcd4b67daeac084b0ed
Alberto <alberto.lopezbarranco@gmail.com>
2018-04-04 09:37:38 +0200

    Added basic tests

--- test/index.js
+++ test/index.js
@@ -1,8 +1,40 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
-    });
+const expect = require("expect.js");
+const esprima = require("esprima").parseScript;
+const jsMigrator = require("../src/js-migrator.js");
+
+const wrapInScript = fragment => `Polymer(${(fragment)})`;
+
+suite("Javascript", function() {
+  test("Converts to class form", function() {
+    const polymerConfig = `{ is: "my-class-name" }`;
+    const fragment = wrapInScript(polymerConfig);
+
+    const expected = `class MyClassName extends Polymer.Element {
+                            static get is() {return 'my-class-name';}
+                      }
+                      window.customElements.define(MyClassName.is, MyClassName);`;
+
+    expect(esprima(jsMigrator.migrate(fragment))).eql(esprima(expected));
+  });
+  test("Adds properties as static getter", function() {
+    const polymerConfig = `{
+      is: "my-class-name",
+      properties:{
+        prop1:String,
+        prop2:Boolean
+      }
+    }`;
+    const fragment = wrapInScript(polymerConfig);
+
+    const expected = `class MyClassName extends Polymer.Element {
+                            static get is() {return 'my-class-name';}
+                            static get properties(){return {
+                              prop1:String,
+                              prop2:Boolean
+                            }}
+                      }
+                      window.customElements.define(MyClassName.is, MyClassName);`;
+
+    expect(esprima(jsMigrator.migrate(fragment))).eql(esprima(expected));
   });
 });
